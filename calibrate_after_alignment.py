10effc65dc27c987f05afbe3849329ab337e540b
IrenevdKruijs <vandekruijsirene@gmail.com>
2025-05-15 16:48:17 +0200

    gave option to not override old document

--- calibrate_after_alignment.py
+++ calibrate_after_alignment.py
@@ -7,11 +7,18 @@ This script finds the middle of the beam and displays those coordinates. This sc
 """
 
 from functions import *
-
+filename = "target_pixels.txt"
 cam=camera_controller()
 img = cam.capture_image()
 target_x, target_y = localize_beam_center(img)
 print(f"target pixel is ({target_x},{target_y})")
-with open("alignment_result.txt", "w") as f:
-    f.write(f"{target_x},{target_y}")
-
+if os.path.exists(filename):
+    overwrite = input(f"The file '{filename}' already exists. Do you want to overwrite it? (yes/no): ").strip().lower()
+    if overwrite == "yes":
+        with open(filename, "w") as f:
+            f.write(f"{target_x},{target_y}")
+    else:
+        print("File not overwritten.")
+else:
+    with open(filename, "w") as f:
+        f.write(f"{target_x},{target_y}")
