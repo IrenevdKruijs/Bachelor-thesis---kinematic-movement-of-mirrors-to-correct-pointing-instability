37dbe72383013a7c714f41cb87da01c874336f75
IrenevdKruijs <vandekruijsirene@gmail.com>
2025-05-21 23:15:29 +0200

    fixed bugs

--- calibrate_after_alignment.py
+++ calibrate_after_alignment.py
@@ -7,8 +7,9 @@ This script finds the middle of the beam and displays those coordinates. This sc
 """
 
 from functions import *
-filename = "target_pixels.txt"
-cam=camera_controller()
+filename = "target_pixels_cam1.txt"
+exposuretime=1000
+cam=camera_controller(exposuretime)
 img = cam.capture_image()
 target_x, target_y = localize_beam_center(img)
 print(f"target pixel is ({target_x},{target_y})")
@@ -22,3 +23,20 @@ if os.path.exists(filename):
 else:
     with open(filename, "w") as f:
         f.write(f"{target_x},{target_y}")
+        
+#implement when using second camera
+# filename = "target_pixels_cam2.txt"
+# cam2=camera_controller()
+# img = cam2.capture_image()
+# target_x, target_y = localize_beam_center(img)
+# print(f"target pixel is ({target_x},{target_y})")
+# if os.path.exists(filename):
+#     overwrite = input(f"The file '{filename}' already exists. Do you want to overwrite it? (yes/no): ").strip().lower()
+#     if overwrite == "yes":
+#         with open(filename, "w") as f:
+#             f.write(f"{target_x},{target_y}")
+#     else:
+#         print("File not overwritten.")
+# else:
+#     with open(filename, "w") as f:
+#         f.write(f"{target_x},{target_y}")
