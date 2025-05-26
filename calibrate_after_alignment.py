a0c83311055bd880c924083491930777c85b0ccd
IrenevdKruijs <vandekruijsirene@gmail.com>
2025-05-26 15:19:20 +0200

    added second mirror to script to define target pixels

--- calibrate_after_alignment.py
+++ calibrate_after_alignment.py
@@ -24,19 +24,18 @@ else:
     with open(filename, "w") as f:
         f.write(f"{target_x},{target_y}")
         
-#implement when using second camera
-# filename = "target_pixels_cam2.txt"
-# cam2=camera_controller()
-# img = cam2.capture_image()
-# target_x, target_y = localize_beam_center(img)
-# print(f"target pixel is ({target_x},{target_y})")
-# if os.path.exists(filename):
-#     overwrite = input(f"The file '{filename}' already exists. Do you want to overwrite it? (yes/no): ").strip().lower()
-#     if overwrite == "yes":
-#         with open(filename, "w") as f:
-#             f.write(f"{target_x},{target_y}")
-#     else:
-#         print("File not overwritten.")
-# else:
-#     with open(filename, "w") as f:
-#         f.write(f"{target_x},{target_y}")
+
+filename = "target_pixels_cam2.txt"
+img2 = cam.capture_image(2)
+target_x2, target_y2 = localize_beam_center(img2)
+print(f"target pixel is ({target_x2},{target_y2})")
+if os.path.exists(filename):
+    overwrite = input(f"The file '{filename}' already exists. Do you want to overwrite it? (yes/no): ").strip().lower()
+    if overwrite == "yes":
+        with open(filename, "w") as f:
+            f.write(f"{target_x},{target_y}")
+    else:
+        print("File not overwritten.")
+else:
+    with open(filename, "w") as f:
+        f.write(f"{target_x},{target_y}")
